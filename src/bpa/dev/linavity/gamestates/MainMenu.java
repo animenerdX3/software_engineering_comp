62697fcd091c4b139f075ed4c6fb8fbfffe1cd4d
saltin3s <33356426+saltin3s@users.noreply.github.com>
2017-11-15 14:08:32 -0500

    Update MainMenu.java

--- src/bpa/dev/linavity/gamestates/MainMenu.java
+++ src/bpa/dev/linavity/gamestates/MainMenu.java
@@ -24,6 +24,10 @@ public class MainMenu extends BasicGameState{
 	private Image bg = null;
 	private Image play = null;
 	private Image exit = null;
+	private Image options = null;
+	private Image back = null;
+	
+	private boolean isOption = false;
 	
 	// This runs as soon as we compile the program.
 	public void init(GameContainer gc, StateBasedGame sbg)
@@ -31,24 +35,38 @@ public class MainMenu extends BasicGameState{
 		
 		// Initialize our image objects
 		bg = new Image("data/bg.jpg"); // Menu Background
+		
+		// Main Menu Buttons
 		play = new Image("data/button_play.png"); // Play Button
 		exit = new Image("data/button_exit.png"); // Exit Button
+		options = new Image("data/button_options.png"); // Options Button
+
+		// Option buttons
+		back = new Image("data/button_back.png"); // Back Button		
 	}
 
 	// Renders content to the game / screen
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
 		
-		// Draw our menu UI:
-		// Background Image
-		// Play Button
-		// Exit Button
-		g.drawImage(bg, 0, 0); // Draw a background
-		g.drawImage(play, (gc.getWidth()/2) - (play.getWidth()/2), 300); // Setting the x value as half of the game container and adjusting for the width of the button
-		g.drawImage(exit, (gc.getWidth()/2) - (play.getWidth()/2), 400); // Setting the x value as half of the game container and adjusting for the width of the button
+		// DRAW OUR MENU UI //
 		
+		// Background Image
+		g.drawImage(bg, 0, 0);
 		g.drawString("XPOS: " + xpos + " | YPOS: " + ypos, 10, 30); // Draw our mouse position for debugging purposes. 
-		g.drawString(""+ (300 + play.getHeight()), 10, 50);
+		
+		// Button Rendering
+		if(!isOption){ 
+			renderMainMenuScreenMain(gc, g); // If the option menu has not been selected, render the main section of the menu
+		}else{ 
+			renderMainMenuScreenOptions(gc, g); // If the option menu is selected, render the options section of the menu
+		}
+		
+		// END OUR MENU UI //
+
+		
+		
+
 	}
 
 	// Constant Loop, very fast, loops based on a delta (the amount of time that passes between each instance)
@@ -65,29 +83,131 @@ public class MainMenu extends BasicGameState{
 		xpos = ExtraMouseFunctions.getMouseX(gc.getWidth()); // Updates the x coordinate of the mouse
 		ypos = ExtraMouseFunctions.getMouseY(gc.getHeight()); // Updates the y coordinate of the mouse
 
+		
+		// CHECK OUR MOUSE INPUT //
+		
+		if(!isOption){
+			mainButtonAction(gc, sbg, input); // If the option menu has not been selected, check for the main section's buttons
+		}else{
+			optionButtonAction(gc, sbg, input); // If the option menu is selected, check for the option section's buttons
+		}
+		
+		// END OUR MOUSE INPUT
+
+	}
+
+
+	/**
+	 * @method renderMainMenuScreenMain
+	 * @description draws the images needed for the main screen of the main menu
+	 * 
+	 * @param
+	 * 	GameContainer gc, Graphics g
+	 * 
+	 * @return
+	 * 	void:
+	 */
+	public void renderMainMenuScreenMain(GameContainer gc, Graphics g){
+		// Play Button 
+		g.drawImage(play, (gc.getWidth()/2) - (play.getWidth()/2), 300); // Setting the x value as half of the game container and adjusting for the width of the button
+		
+		// Options Button
+		g.drawImage(options, (gc.getWidth()/2) - (play.getWidth()/2), 400); // Setting the x value as half of the game container and adjusting for the width of the button
+		
+		// Exit Button
+		g.drawImage(exit, (gc.getWidth()/2) - (play.getWidth()/2), 500); // Setting the x value as half of the game container and adjusting for the width of the button
+	}
+	
+
+	/**
+	 * @method mainButtonAction
+	 * @description checks the button detection and handles the according events in the main section of our main menu
+	 * 
+	 * @param
+	 * GameContainer gc
+	 * 
+	 * @return
+	 * 	void:
+	 * @throws SlickException 
+	 */
+	public void mainButtonAction(GameContainer gc, StateBasedGame sbg, Input input) 
+			throws SlickException{
+		
+		play = new Image("data/button_play.png");
+		options = new Image("data/button_options.png");
+		exit = new Image("data/button_exit.png");
+
+		// Play Button
 		// The parameters for checkbounds are the x and y coordinates of the top left of the button and the bottom right of the button
 		if(checkBounds( (gc.getWidth()/2) - (play.getWidth()/2) , (gc.getWidth()/2) - (play.getWidth()/2) + play.getWidth() , 300 , 300 + play.getHeight())) {
-			if(input.isMouseButtonDown(0)){		
+			if(input.isMousePressed(0)){		
 				input.clearKeyPressedRecord();
 				sbg.enterState(1);
 			}
+			play = new Image("data/button_play_hover.png");
 		}
 		
+		// Options Button
 		// The parameters for checkbounds are the x and y coordinates of the top left of the button and the bottom right of the button
-		if(checkBounds( (gc.getWidth()/2) - (exit.getWidth()/2) , (gc.getWidth()/2) - (exit.getWidth()/2) + exit.getWidth() , 400 , 400 + exit.getHeight())){
-			if(input.isMouseButtonDown(0)){
+		if(checkBounds( (gc.getWidth()/2) - (options.getWidth()/2) , (gc.getWidth()/2) - (options.getWidth()/2) + options.getWidth() , 400 , 400 + options.getHeight())){
+			if(input.isMousePressed(0)){
+				isOption = true;
+			}
+			options = new Image("data/button_options_hover.png");
+		}
+		
+		// Exit Button
+		// The parameters for checkbounds are the x and y coordinates of the top left of the button and the bottom right of the button
+		if(checkBounds( (gc.getWidth()/2) - (exit.getWidth()/2) , (gc.getWidth()/2) - (exit.getWidth()/2) + exit.getWidth() , 500 , 500 + exit.getHeight())){
+			if(input.isMousePressed(0)){
 				System.exit(0);
 			}
+			exit = new Image("data/button_exit_hover.png");
 		}
-
-	}
-
-	// Return the gamestate ID
-	public int getID() {
-		return MainMenu.id;
 	}
 	
-	//TODO things
+	/**
+	 * @method renderMainMenuScreenOptions
+	 * @description draws the images needed for the main screen of the main menu
+	 * 
+	 * @param
+	 * GameContainer gc, Graphics g
+	 * 
+	 * @return
+	 * 	void:
+	 */
+	public void renderMainMenuScreenOptions(GameContainer gc, Graphics g){
+		// Back Button
+		g.drawImage(back, (gc.getWidth()/2) - (back.getWidth()/2), 400); // Setting the x value as half of the game container and adjusting for the width of the button
+	}
+
+	/**
+	 * @method optionButtonAction
+	 * @description checks the button detection and handles the according events in the options section of our main menu
+	 * 
+	 * @param
+	 * GameContainer gc
+	 * 
+	 * @return
+	 * 	void:
+	 * @throws SlickException 
+	 */
+	public void optionButtonAction(GameContainer gc, StateBasedGame sbg, Input input) 
+			throws SlickException{
+		
+		back = new Image("data/button_back.png");
+		
+		// Back Button
+		// The parameters for checkbounds are the x and y coordinates of the top left of the button and the bottom right of the button
+		if(checkBounds( (gc.getWidth()/2) - (back.getWidth()/2) , (gc.getWidth()/2) - (back.getWidth()/2) + back.getWidth() , 400 , 400 + back.getHeight())){
+			if(input.isMousePressed(0)){
+				isOption = false;
+			}
+			back = new Image("data/button_back_hover.png");
+		}
+		
+	}
+	
 	
 	/**
 	 * @method checkBounds
@@ -99,11 +219,24 @@ public class MainMenu extends BasicGameState{
 	 * @return
 	 * 	boolean: Whether or not the mouse is in the bounds
 	 */
-	private boolean checkBounds(int x1, int x2, int y1, int y2){
+	boolean checkBounds(int x1, int x2, int y1, int y2){
 		if((xpos > x1 && xpos < x2) && (ypos > y1 && ypos < y2 )){
 			return true;
 		}
 		return false;
+	}
+	
+	// For other classes to use the checkBounds method
+	static boolean checkBounds(int x1, int x2, int y1, int y2, int xPosition, int yPosition){
+		if((xPosition > x1 && xPosition < x2) && (yPosition > y1 && yPosition < y2 )){
+			return true;
+		}
+		return false;
+	}
+	
+	// Return the gamestate ID
+	public int getID() {
+		return MainMenu.id;
 	}
 	
 }
