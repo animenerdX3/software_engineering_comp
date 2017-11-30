8c54eb7e1d0bdce79aec0f04a9764e7cbaf056a6
yalmeida <33158563+yalmeida@users.noreply.github.com>
2017-11-30 13:30:42 -0500

    Update Player.java

--- src/bpa/dev/linavity/entities/Player.java
+++ src/bpa/dev/linavity/entities/Player.java
@@ -1,15 +1,46 @@
 package bpa.dev.linavity.entities;
 
-import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class Player extends Mob {
-
+	
 	public Player() 
 			throws SlickException{
 		super();
 	}
 	
-	
+	public void updatePos(boolean[] keyLog, int delta) {
+		
+		/*
+		 *MOVEMENT
+		 * Keys
+		 * 	W - Up (0)
+		 * 	A - Left (1)
+		 *  S - Down (2)
+		 *  D - Right (3)
+		 *  
+		 * Left-Shift (4) 
+		 */
+		
+		if(keyLog[0]) //Check For W Key
+			this.setY((int) (getY() - 200/1000.0f * delta));
+		
+		if(keyLog[1] && keyLog[4])  //Check For Run :  A + Left-Shift Pressed Together
+			this.setX( (int) (getX() - ((200/1000.0f * delta) * 1.7)));
+		else if(keyLog[1]) //Check For A Key
+			this.setX((int) (getX() - 200/1000.0f * delta));
+		
+		if(keyLog[2]) //Check For S Key
+			this.setY((int) (getY() + 200/1000.0f * delta));
+		
+		if(keyLog[3] && keyLog[4])  //Check For Run :  D + Left-Shift Pressed Together
+			this.setX( (int) (getX() + ((200/1000.0f * delta) * 1.7)));
+		else if(keyLog[3]) //Check For D key
+			this.setX((int) (getX() + 200/1000.0f * delta));
+		
+
+		super.updatePos();
+		
+	}
 	
 }//end of class
