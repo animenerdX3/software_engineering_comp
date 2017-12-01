3ffd05f9b9b46fdd6573fb70fc231dd2f6440cf1
Peter Gomes <petergomesbusiness@gmail.com>
2017-12-01 14:25:48 -0500

    Instance variable given to Collision class

--- src/bpa/dev/linavity/assets/Collision.java
+++ src/bpa/dev/linavity/assets/Collision.java
@@ -2,4 +2,32 @@ package bpa.dev.linavity.assets;
 
 public class Collision {
 
-}//class of class
+	//Instance variables
+	private boolean collide;
+	
+	//Default constructor
+	public Collision() {
+		this.collide = true;
+	}
+	
+	//Non-Default Constructor
+	public Collision(boolean collide) {
+		this.collide = collide;
+	}
+	
+	/* GETTERS */
+	public boolean getCollide() {
+		return collide;
+	}
+	
+	/* SETTERS */
+	public void setCollide(boolean collide) {
+		this.collide = collide;
+	}
+	
+	//Overrides toString method
+	public String toString() {
+		return "Collide: "+this.collide;
+	}
+	
+}//end of class
