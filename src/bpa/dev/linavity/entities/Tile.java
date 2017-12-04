36d4dc3875a4e9b9e07c85b4055f114c3d4270e6
saltin3s <33356426+saltin3s@users.noreply.github.com>
2017-12-04 09:41:41 -0500

    Update Tile.java

--- src/bpa/dev/linavity/entities/Tile.java
+++ src/bpa/dev/linavity/entities/Tile.java
@@ -1,5 +1,33 @@
 package bpa.dev.linavity.entities;
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
 public class Tile {
 
+	// Tile ID
+	int id;
+	
+	// X and Y position of the tile in the level (not relative position)
+	int x, y;
+	
+	// Width and height of the tiles (Right now, typically 64x64)
+	int height, width;
+	
+	// the image of our tile
+	private Image texture = null;
+	
+	// Possible paths for our image files
+	String[] texturePaths = {"res/floor.png", "res/floor_rust1.png", "res/floor_rust2.png"};
+	
+	public Tile(int x, int y, int id) 
+			throws SlickException{
+		this.height = 64;
+		this.width = 64;
+		this.x = x;
+		this.y = y;
+		this.id = id;
+		this.texture = new Image(texturePaths[id]);
+	}
+	
 }
