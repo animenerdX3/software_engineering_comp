14b408d848aa1d11d30a111a431f313d4c82b28e
Peter Gomes <petergomesbusiness@gmail.com>
2017-12-05 09:56:58 -0500

    Update Mob.java

--- src/bpa/dev/linavity/entities/Mob.java
+++ src/bpa/dev/linavity/entities/Mob.java
@@ -13,25 +13,34 @@ public class Mob{
 	private Image mobImage = null;
 	private Collision collide;
 	private float ymo; // Momentum in the y direction
-	
-	
 
-
-
+	//Default constructor
 	public Mob() 
 			throws SlickException{
-		setMobImage(new Image("data/player_0.png"));
+		setMobImage(new Image("data/player_0.png"));//By default, our Mob has the player skin
+		ymo = 0;
+		this.collide = new Collision();
+	}
+	
+	//Non-Default constructor
+	public Mob(String textureDirectory) 
+			throws SlickException{
+		setMobImage(new Image(textureDirectory));//Give our mob a skin that we pre-determined
 		ymo = 0;
 		this.collide = new Collision();
 	}
 
-	
+	/**
+	 * Update our gravity based on our y momentum
+	 */
 	public void updatePos() {
 		//Gravity affecting the player
 		this.setY((int) (this.getY() + gravity.getGravity() - ymo));
 		if(ymo >= 0)
 			ymo -= .7;
 	}
+	
+	/* GETTERS */
 	
 	/**
 	 * @return the x
@@ -40,12 +49,6 @@ public class Mob{
 		return x;
 	}
 
-	/**
-	 * @param x the x to set
-	 */
-	public  void setX(int x) {
-		this.x = x;
-	}
 
 	/**
 	 * @return the y
@@ -55,28 +58,37 @@ public class Mob{
 	}
 
 	/**
-	 * @param y the y to set
-	 */
-	public  void setY(int y) {
-		this.y = y;
-	}
-
-	/**
 	 * @return the mobImage
 	 */
 	public  Image getMobImage() {
 		return mobImage;
 	}
+	
+	public float getYmo() {
+		return ymo;
+	}
+	
+	/* SETTERS */
+	
+	/**
+	 * @param x the x to set
+	 */
+	public  void setX(int x) {
+		this.x = x;
+	}
 
+	/**
+	 * @param y the y to set
+	 */
+	public  void setY(int y) {
+		this.y = y;
+	}
+	
 	/**
 	 * @param mobImage the mobImage to set
 	 */
 	public void setMobImage(Image mobImage) {
 		this.mobImage = mobImage;
-	}
-	
-	public float getYmo() {
-		return ymo;
 	}
 
 
