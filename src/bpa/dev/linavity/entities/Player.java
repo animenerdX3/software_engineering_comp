69c6182975005a8f04380e3b1bbfafb8135cc439
Peter Gomes <petergomesbusiness@gmail.com>
2017-12-05 09:57:47 -0500

    Documented Code
    uwu

--- src/bpa/dev/linavity/entities/Player.java
+++ src/bpa/dev/linavity/entities/Player.java
@@ -1,32 +1,47 @@
-package bpa.dev.linavity.physics;
+package bpa.dev.linavity.entities;
 
 import org.newdawn.slick.SlickException;
 
-public class Gravity {
+public class Player extends Mob {
 	
-	public int gravityPower; // The factor by which objects in our world are moved by gravity/antigravity
-
-	public Gravity() throws SlickException {
-		this.gravityPower = 4; // The initial state of gravity
+	public Player() 
+			throws SlickException{
+		super();
 	}
 	
-	/* GETTERS */
-	
-	public int getGravity() {
-		return gravityPower;
+	public void updatePos(boolean[] keyLog, int delta) {
+		
+		/*
+		 *MOVEMENT
+		 * Keys
+		 * 	W - Up (0)
+		 * 	A - Left (1)
+		 *  S - Down (2)
+		 *  D - Right (3)
+		 *  
+		 * Left-Shift (4) 
+		 */
+		
+		if(keyLog[0]) //Check For W Key
+			jump();
+		
+		if(keyLog[1] && keyLog[4])  //Check For Run :  A + Left-Shift Pressed Together
+			this.setX( (int) (getX() - ((200/1000.0f * delta) * 1.7)));
+		else if(keyLog[1]) //Check For A Key
+			this.setX((int) (getX() - 200/1000.0f * delta));
+		
+		if(keyLog[3] && keyLog[4])  //Check For Run :  D + Left-Shift Pressed Together
+			this.setX( (int) (getX() + ((200/1000.0f * delta) * 1.7)));
+		else if(keyLog[3]) //Check For D key
+			this.setX((int) (getX() + 200/1000.0f * delta));
+				
+		super.updatePos();
+		
 	}
 	
-	/* SETTERS */
-	
-	public void setGravity(int gravity){
-		this.gravityPower = gravity;
-	}
-	
-	/**
-	 *  Used to flip gravity
-	 */
-	public void flipGravity(){
-		this.gravityPower = -this.gravityPower;
+	// Player jumping function
+	public void jump(){
+		this.setYmo(14);//Sets Y-Momentum to 14, this makes the player fight against gravity
 	}
 	
 }//end of class
