fc8c2ec684cf58300aee3b026d9655fb1d99cbbf
eg21766 <eg21766@C102-2S6D.GNBVT.LOCAL>
2018-02-02 09:08:34 -0500

    Added Spike Class

---
+++ src/bpa/dev/linavity/entities/tiles/interactive/Spikes.java
@@ -0,0 +1,61 @@
+package bpa.dev.linavity.entities.tiles.interactive;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+import bpa.dev.linavity.GameObject;
+import bpa.dev.linavity.Main;
+import bpa.dev.linavity.entities.Player;
+import bpa.dev.linavity.entities.enemies.Starter;
+import bpa.dev.linavity.entities.tiles.Dynamic;
+import bpa.dev.linavity.events.Message;
+import bpa.dev.linavity.gamestates.GameLevel;
+
+public class Spikes extends Dynamic {
+	
+	
+	public Spikes(int i, int j, int id, int xOffset, int yOffset, int width, int height) throws SlickException {
+		super(i, j, id, xOffset, yOffset, width, height);
+	}
+	
+	@Override
+	public void onCollide(GameObject go) throws SlickException {
+
+		
+		
+		if(go instanceof Player){ // If the mob colliding with the lever was the player
+			
+			if(Main.util.getKeyLogSpecificKey(8)){ // and the player hits e (interact)
+				
+				if(this.toggle){ // If the lever was on
+					toggleOff();
+				}else{
+					toggleOn();
+				}
+			}
+		}
+	}
+	
+	public void toggleOn() throws SlickException {
+		this.setTexture(new Image("res/tiles/static/Lever_On.png"));
+		this.toggle = !this.toggle;
+		System.out.println("ON");
+		
+		for(int i = 0; i < this.targetObjects.length; i++) {
+			Main.util.getMessageHandler().addMessage(new Message(Main.util.getLevel().getSingleEventTile(targetObjects[i]), this, Message.leverToggle, this.toggle));
+		}
+		
+			
+	}
+	
+	public void toggleOff() throws SlickException {
+		this.setTexture(new Image("res/tiles/static/Lever_Off.png"));
+		this.toggle = !this.toggle;
+		System.out.println("OFF");
+		
+		for(int i = 0; i < this.targetObjects.length; i++) {
+			Main.util.getMessageHandler().addMessage(new Message(Main.util.getLevel().getSingleEventTile(targetObjects[i]), this, Message.leverToggle, this.toggle));
+		}
+	}
+	
+}
